b9bcde56982702cb77dd2d5f3591700567b55927
SFedulov23 <SFedulov@mlg.ru>
2024-06-06 02:27:04 +0300

    fix

--- use/change.py
+++ use/change.py
@@ -3,27 +3,27 @@ class Change:
     b = "blue"
     c = "white"
 
-    d = "black"
+    d = "rad"
     e = "yellow"
-    f = "rad"
+    f = "black"
 
     color = {
-        "a": d,
-        "b": e,
-        "c": f,
+        a: d,
+        b: e,
+        c: f,
 
-        "d": a,
-        "e": b,
-        "f": c,
+        d: a,
+        e: b,
+        f: c,
     }
 
     def change(self, item):
         match item:
-            case "a":
+            case Change.a:
                 return Change.d
-            case "b":
+            case Change.b:
                 return Change.e
-            case "c":
+            case Change.c:
                 return Change.f
 
     def change_2(self, item):
@@ -32,5 +32,5 @@ class Change:
 
 if __name__ == '__main__':
     c = Change()
-    print(c.change("b"))
-    print(c.change_2("a"))
+    print(c.change("grin"))
+    print(c.change_2("blue"))
